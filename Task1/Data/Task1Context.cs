ac09c5af7e8f3f2723e17c9d16a10c18f3414a40
GlebPractica <glebkpjp@gmail.com>
2024-05-23 10:04:05 +0300

    Пратика 2.10

---
+++ Task1/Data/Task1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Task1.Models;
+
+namespace Task1.Data
+{
+    public class Task1Context : DbContext
+    {
+        public Task1Context (DbContextOptions<Task1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Task1.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Task1/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Task1.Data;
+using Task1.Models;
+
+namespace Task1.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Task1.Data.Task1Context _context;
+
+        public IndexModel(Task1.Data.Task1Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
